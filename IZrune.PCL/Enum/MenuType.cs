0e2cf2d46e22be20557a8fac1232e4b5d195ca49
beqa149 <beqabaramia14@gmail.com>
2019-05-17 12:27:17 +0400

    asd

---
+++ IZrune.PCL/Enum/MenuType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IZrune.PCL.Enum
+{
+    public enum MenuType
+    {
+        Main,
+        LogIn,
+        MoreInfo,
+        Statistic,
+        UpdatePacket,
+        EditProfile,
+        News,
+        Contact,
+        LogOut
+    }
+}
---
+++ Izrune.iOS/ViewControllers/MenuRootViewController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using IZrune.PCL.Enum;
+using SidebarNavigation;
+using UIKit;
+
+namespace Izrune.iOS.ViewControllers
+{
+    public partial class MenuRootViewController : UIViewController
+    {
+
+        UIStoryboard _storyBoard;
+
+        public SidebarController SideBarController { get; private set; }
+
+        Dictionary<MenuType, Func<UIViewController>> menuViewControllerCreations;
+
+        public MenuRootViewController()
+        {
+            _storyBoard = UIStoryboard.FromName("Main", null);
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
