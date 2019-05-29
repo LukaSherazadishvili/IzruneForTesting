d4b8209c88f0989e075e782524aef664522f081a
beqa149 <beqabaramia14@gmail.com>
2019-05-29 13:05:42 +0400

    asd

---
+++ Izrune.iOS/CollectionViewCells/PacketCollectionViewCell.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Foundation;
+using UIKit;
+
+namespace Izrune.iOS.CollectionViewCells
+{
+    public partial class PacketCollectionViewCell : UICollectionViewCell
+    {
+        public static readonly NSString Key = new NSString("PacketCollectionViewCell");
+        public static readonly UINib Nib;
+
+        static PacketCollectionViewCell()
+        {
+            Nib = UINib.FromName("PacketCollectionViewCell", NSBundle.MainBundle);
+        }
+
+        protected PacketCollectionViewCell(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+    }
+}
