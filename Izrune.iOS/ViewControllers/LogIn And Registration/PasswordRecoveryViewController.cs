e859676c25bca5bd7d381fe58f4436e1af5460a3
beqa149 <beqabaramia14@gmail.com>
2019-06-03 15:28:39 +0400

    asd

--- Izrune.iOS/ViewControllers/LogIn And Registration/PasswordRecoveryViewController.cs
+++ Izrune.iOS/ViewControllers/LogIn And Registration/PasswordRecoveryViewController.cs
@@ -36,6 +36,18 @@ namespace Izrune.iOS
                 ShowError(IsError);
                 IsError = !IsError;
                 this.View.EndEditing(true);
+
+                var succsessVc = Storyboard.InstantiateViewController(SuccesViewController.StoryboardId) as SuccesViewController;
+                succsessVc.TitleText = "პაროლი გაგზავნილი მითითებულ ნომერზე";
+
+                this.AddChildViewController(succsessVc);
+
+
+                succsessVc.View.Frame = new CoreGraphics.CGRect(0, 0, View.Frame.Width, View.Frame.Height);
+
+                View.AddSubview(succsessVc.View);
+
+                succsessVc.DidMoveToParentViewController(this);
             };
 
             backBtn.TouchUpInside += delegate {
