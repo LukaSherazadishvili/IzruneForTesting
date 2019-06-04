366d415734f20ef938580f02bbe2f6e4bbe129c7
beqa149 <beqabaramia14@gmail.com>
2019-06-04 14:15:45 +0400

    added test result cell

---
+++ Izrune.iOS/CollectionViewCells/ResultCollectionViewCell.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Foundation;
+using UIKit;
+
+namespace Izrune.iOS.CollectionViewCells
+{
+    public partial class ResultCollectionViewCell : UICollectionViewCell
+    {
+        public static readonly NSString Key = new NSString("ResultCollectionViewCell");
+        public static readonly UINib Nib;
+
+        public static readonly NSString Identifier = new NSString("ResultCellIdentifier");
+
+        static ResultCollectionViewCell()
+        {
+            Nib = UINib.FromName("ResultCollectionViewCell", NSBundle.MainBundle);
+        }
+
+        protected ResultCollectionViewCell(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+
+    }
+}
