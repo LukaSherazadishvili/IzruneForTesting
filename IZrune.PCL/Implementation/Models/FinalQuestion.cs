b2eff7ba0d80c914696d8b6e874fb360eb7ab0eb
DESKTOP-OVRURPE\Levani <Levanmacharashvilii@gmail.com>
2019-06-07 05:11:06 -0700

    Commit

--- IZrune.PCL/Implementation/Models/FinalQuestion.cs
+++ IZrune.PCL/Implementation/Models/FinalQuestion.cs
@@ -8,7 +8,8 @@ namespace IZrune.PCL.Implementation.Models
     public class FinalQuestion : IFinalQuestion
     {
         public string Title { get; set; }
-        
+        public int StudentAnswerIndex { get; set; }
+       
         public IEnumerable<string> Images { get; set; }
         public IEnumerable<IFinalAnswers> Answers { get; set; }
     }
