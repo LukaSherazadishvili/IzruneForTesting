842b411220f5d557025a551f3c188fe0298ebfce
Beqa Baramia <beqabaramia@Beqas-iMac.local>
2019-06-12 00:32:16 +0400

    asd

---
+++ Izrune.iOS/CollectionViewCells/DiplomeCollectionViewCell.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Foundation;
+using UIKit;
+
+namespace Izrune.iOS.CollectionViewCells
+{
+    public partial class DiplomeCollectionViewCell : UICollectionViewCell
+    {
+        public static readonly NSString Key = new NSString("DiplomeCollectionViewCell");
+        public static readonly UINib Nib;
+
+        public static readonly NSString Identifier = new NSString("DiplomeCellIdentifier");
+        static DiplomeCollectionViewCell()
+        {
+            Nib = UINib.FromName("DiplomeCollectionViewCell", NSBundle.MainBundle);
+        }
+
+        protected DiplomeCollectionViewCell(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void AwakeFromNib()
+        {
+            base.AwakeFromNib();
+
+            mainView.Layer.BorderWidth = 2;
+            mainView.Layer.BorderColor = UIColor.White.CGColor;
+
+            mainView.Layer.CornerRadius = 20;
+        }
+    }
+}
