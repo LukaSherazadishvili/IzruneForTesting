7fd04257d461ebbef5cdf4774803d8e6daef955d
beqa149 <beqabaramia14@gmail.com>
2019-06-27 15:07:55 +0400

    asd

--- Izrune.iOS/ViewControllers/LogIn And Registration/AddStudentViewController.designer.cs
+++ Izrune.iOS/ViewControllers/LogIn And Registration/AddStudentViewController.designer.cs
@@ -1,62 +1,90 @@
 // WARNING
 //
-// This file has been generated automatically by Visual Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
 using Foundation;
-using System;
 using System.CodeDom.Compiler;
 
 namespace Izrune.iOS
 {
-    [Register ("AddStudentViewController")]
-    partial class AddStudentViewController
-    {
-        [Outlet]
-        UIKit.UIButton addNewStudentBtn { get; set; }
+	[Register ("AddStudentViewController")]
+	partial class AddStudentViewController
+	{
+		[Outlet]
+		UIKit.UIButton addNewStudentBtn { get; set; }
 
+		[Outlet]
+		UIKit.UIView agreeView { get; set; }
 
-        [Outlet]
-        UIKit.UIView agreeView { get; set; }
+		[Outlet]
+		UIKit.UIImageView arrowImageView { get; set; }
 
+		[Outlet]
+		UIKit.UIButton backBtn { get; set; }
 
-        [Outlet]
-        UIKit.UIButton backBtn { get; set; }
+		[Outlet]
+		UIKit.UILabel finishLbl { get; set; }
 
+		[Outlet]
+		UIKit.UIButton nextBtn { get; set; }
 
-        [Outlet]
-        UIKit.UIButton nextBtn { get; set; }
+		[Outlet]
+		UIKit.UILabel privacyLbl { get; set; }
 
+		[Outlet]
+		UIKit.UIStackView privacyStackView { get; set; }
 
-        [Outlet]
-        UIKit.UILabel privacyLbl { get; set; }
+		[Outlet]
+		UIKit.UIView selectedAgreeView { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (addNewStudentBtn != null) {
+				addNewStudentBtn.Dispose ();
+				addNewStudentBtn = null;
+			}
 
+			if (agreeView != null) {
+				agreeView.Dispose ();
+				agreeView = null;
+			}
 
-        [Outlet]
-        UIKit.UIView selectedAgreeView { get; set; }
+			if (backBtn != null) {
+				backBtn.Dispose ();
+				backBtn = null;
+			}
 
-        void ReleaseDesignerOutlets ()
-        {
-            if (addNewStudentBtn != null) {
-                addNewStudentBtn.Dispose ();
-                addNewStudentBtn = null;
-            }
+			if (nextBtn != null) {
+				nextBtn.Dispose ();
+				nextBtn = null;
+			}
 
-            if (agreeView != null) {
-                agreeView.Dispose ();
-                agreeView = null;
-            }
+			if (privacyLbl != null) {
+				privacyLbl.Dispose ();
+				privacyLbl = null;
+			}
 
-            if (privacyLbl != null) {
-                privacyLbl.Dispose ();
-                privacyLbl = null;
-            }
+			if (selectedAgreeView != null) {
+				selectedAgreeView.Dispose ();
+				selectedAgreeView = null;
+			}
 
-            if (selectedAgreeView != null) {
-                selectedAgreeView.Dispose ();
-                selectedAgreeView = null;
-            }
-        }
-    }
+			if (finishLbl != null) {
+				finishLbl.Dispose ();
+				finishLbl = null;
+			}
+
+			if (privacyStackView != null) {
+				privacyStackView.Dispose ();
+				privacyStackView = null;
+			}
+
+			if (arrowImageView != null) {
+				arrowImageView.Dispose ();
+				arrowImageView = null;
+			}
+		}
+	}
 }
