167d70ffc6add583272e2d7f34ebbce36ed28b73
beqa149 <beqabaramia14@gmail.com>
2019-07-22 13:21:45 +0400

    asd

---
+++ Izrune.iOS/CollectionViewCells/BadgeCell.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Foundation;
+using UIKit;
+
+namespace Izrune.iOS.CollectionViewCells
+{
+    public partial class BadgeCell : UICollectionViewCell
+    {
+        public static readonly NSString Key = new NSString("BadgeCell");
+        public static readonly UINib Nib;
+
+        static BadgeCell()
+        {
+            Nib = UINib.FromName("BadgeCell", NSBundle.MainBundle);
+        }
+
+        protected BadgeCell(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+    }
+}
