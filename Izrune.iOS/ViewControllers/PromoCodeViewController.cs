c4cb719c4969d495d59986dfff43f3755d1548d6
beqa149 <beqabaramia14@gmail.com>
2019-08-20 13:38:25 +0400

    bugfix

--- Izrune.iOS/ViewControllers/PromoCodeViewController.cs
+++ Izrune.iOS/ViewControllers/PromoCodeViewController.cs
@@ -154,8 +154,15 @@ namespace Izrune.iOS
 
                     //SelectedPrice.MonthCount = PromoInfo.Prices.ElementAt((int)index).price.Value;
                     //SelectedPrice.price = PromoInfo.Prices.ElementAt((int)index).MonthCount.Value;
-                    UserControl.Instance.CurrentStudent.Amount = PromoInfo.Prices.ElementAt((int)index).price.Value;
-                    UserControl.Instance.CurrentStudent.PackageMonthCount = PromoInfo.Prices.ElementAt((int)index).MonthCount.Value;
+
+                    var currStudent = UserControl.Instance.CurrentStudent;
+
+                    if(currStudent != null)
+                    {
+                        UserControl.Instance.CurrentStudent.Amount = PromoInfo.Prices.ElementAt((int)index).price.Value;
+                        UserControl.Instance.CurrentStudent.PackageMonthCount = PromoInfo.Prices.ElementAt((int)index).MonthCount.Value;
+                    }
+
                     if (IsPeriodSelected && IsPromoValid)
                     {
                         SelectPromoPack(index);
